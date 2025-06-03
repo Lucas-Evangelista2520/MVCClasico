a80bbe26976a89eb75203074595c7f4a6300799b
lucas evangelista <lucasevangelista2520@gmail.com>
2025-06-03 18:07:58 -0300

    Productos bottom solved in Layout.cshtml

--- obj/Debug/net8.0/MVCClasico.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVCClasico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCClasico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ca48966a587aa098c66409ea1f43ded931604e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCClasico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCClasico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
