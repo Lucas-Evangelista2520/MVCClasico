fe62faf08f2effcf2f79ef524a7dc0b31dc517f8
Florencia Amado <amadoflorencia8@gmail.com>
2025-06-23 21:21:40 -0300

    update appsettings.json

--- obj/Debug/net8.0/MVCClasico.AssemblyInfo.cs
+++ obj/Debug/net8.0/MVCClasico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCClasico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7d6f8b7772944b8d20fa0bcd845d1086bbbcf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e21cfd85945db7bf74398b4875c13409259fef6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCClasico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCClasico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
